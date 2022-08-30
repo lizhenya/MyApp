21c5e11876cc2689e1c444b7d4556eb3b2ddeada
lzy <lzy_tinyjoy@163.com>
2022-08-30 17:28:23 +0800

    “test”

--- app/src/main/java/com/lzy/myapp/MainActivity.java
+++ app/src/main/java/com/lzy/myapp/MainActivity.java
@@ -21,18 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE},  122);
-        test();
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void test() {
-        NetCallback networkCallback = new NetCallback();
-        NetworkRequest.Builder builder = new NetworkRequest.Builder();
-        NetworkRequest request = builder.build();
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connMgr != null) {
-            connMgr.registerNetworkCallback(request, networkCallback);
-        }
+//        NetCallback networkCallback = new NetCallback();
+//        NetworkRequest.Builder builder = new NetworkRequest.Builder();
+//        NetworkRequest request = builder.build();
+//        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if (connMgr != null) {
+//            connMgr.registerNetworkCallback(request, networkCallback);
+//        }
     }
 }
